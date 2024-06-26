b51eedfbb0d4aaabf4cea624c2a8c3adced4cad5
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-26 16:57:29 +0000

    pycodestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,6 +11,8 @@ The data will be represented by a list of integers
 Each integer represents 1 byte of data, therefore you only need to handle the
 8 least significant bits of each integer
 '''
+
+
 def validUTF8(data) -> bool:
     """Determines if a given data set represents a valid UTF-8 encoding"""
     num_bytes = 0
@@ -27,7 +29,8 @@ def validUTF8(data) -> bool:
                 num_bytes = 2
             elif (byte >> 3) == 0b11110:  # 4-byte character
                 num_bytes = 3
-            elif (byte >> 7):  # 1-byte character (0xxxxxxx), valid if MSB is set to 1.
+            # 1-byte character (0xxxxxxx), valid if MSB is set to 1.
+            elif (byte >> 7):
                 return False
         else:
             # Continuation byte must be of the form 10xxxxxx
