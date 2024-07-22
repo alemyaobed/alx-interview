5348c1faa224ac52c4ca1ddb1f25162eb3a301c4
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-22 09:19:33 +0000

    the implementation of the function to rotate a 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
+
+jessevhedden$
+jessevhedden$ ./main_0.py
+[[7, 4, 1],
+[8, 5, 2],
+[9, 6, 3]]
+jessevhedden$
+'''
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates an n x n 2D matrix 90 degrees clockwise.
+
+    :param matrix: 2D list of integers
+    """
+    # Transpose the matrix
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
