693edceb6b70c6b74e9988ee06e5e37123c05c96
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-08-08 11:59:19 +0000

    reverting

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,34 +1,47 @@
 #!/usr/bin/python3
-"""Prime Game."""
+""" Prime Game """
 
+def is_prime(n):
+    """Check if a number is prime."""
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def get_prime_counts_up_to(n):
+    """Calculate number of primes up to each number from 1 to n."""
+    prime_count = [0] * (n + 1)
+    count = 0
+    for i in range(2, n + 1):
+        if is_prime(i):
+            count += 1
+        prime_count[i] = count
+    return prime_count
 
 def isWinner(x, nums):
-    """function that checks for the winner"""
-    if not nums or x < 1:
+    """Determine the winner of the most rounds."""
+    if x <= 0 or not nums:
         return None
-    max_num = max(nums)
 
-    filter = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not filter[i]:
-            continue
-        for j in range(i * i, max_num + 1, i):
-            filter[j] = False
-    filter[0] = filter[1] = False
-    y = 0
-    for i in range(len(filter)):
-        if filter[i]:
-            y += 1
-        filter[i] = y
-    player1 = 0
-    for x in nums:
-        player1 += filter[x] % 2 == 1
-    if player1 * 2 == len(nums):
-        return None
-    if player1 * 2 > len(nums):
+    max_n = max(nums)
+    prime_count = get_prime_counts_up_to(max_n)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        # Maria wins if the number of primes up to n is odd, otherwise Ben wins
+        if prime_count[n] % 2 != 0:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
-    return "Ben"
-
-
-if __name__ == "__main__":
-    assert isWinner(5, [2, 5, 1, 4, 3]) == "Ben", "Expected Ben"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        # Return None if the winner cannot be determined
+        return None
